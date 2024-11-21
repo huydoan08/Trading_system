465833326cad4195e8ed86c551458a814baa02c7
huy.nguyen <huy.nguyen8@sotatek.com>
2024-11-21 15:49:54 +0700

    fix

--- src/pages/ImportantNotes/index.jsx
+++ src/pages/ImportantNotes/index.jsx
@@ -5,25 +5,22 @@ import ItemLists from "../../components/ItemLists";
 export default function ImporttantNotes() {
   const data = [
     {
-      info: "Mọi thông tin ở đây chỉ mang tính tham khảo, không chắc chắn một điều gì cả vì kịch bản luôn thay đổi để phù hợp với thị trường.",
-    },
-    {
-      info: "Chúng ta phải thừa nhận rằng thị trường này là một thị trường làm giá, và chúng ta chỉ follow theo dựa vào những dấu hiệu.",
+      info: "Chúng ta phải thừa nhận rằng thị trường này là một thị trường làm giá và chúng ta chỉ follow theo dựa vào những dấu hiệu.",
     },
     {
       info: "Những dấu hiệu thì luôn có tính xác suất, không chắc chắn một điều gì cả.",
     },
     {
-      info: "Việc quan trọng nhất là KĨ NĂNG XỬ LÝ chứ không phải là dự đoán giá",
+      info: "Việc quan trọng nhất là KĨ NĂNG XỬ LÝ chứ không phải là dự đoán giá.",
     },
     {
-      info: "Bạn phải xây dựng cho mình một HỆ THỐNG GIAO DỊCH và BẠN LÀM THEO HỆ THỐNG ĐÓ",
+      info: "Bạn phải xây dựng cho mình một HỆ THỐNG GIAO DỊCH và BẠN LÀM THEO HỆ THỐNG ĐÓ một cách nhất quán.",
     },
     {
-      info: "Cần một thời gian dài mới có thể hiểu được, mới có thể thành thạo được một cái NGHỀ",
+      info: "Cần một thời gian dài mới có thể hiểu được, mới có thể thành thạo được một cái NGHỀ.",
     },
     {
-      info: "Kiên nhẫn và thành thục phương pháp, bạn sẽ nhận được phần thưởng xứng đáng",
+      info: "Kiên nhẫn và thành thục phương pháp, bạn sẽ nhận được phần thưởng xứng đáng.",
     },
     {
       info: "Đứng ngoài chờ đợi khi cơ hội chưa thực sự rõ ràng cũng đã là thành công.",
