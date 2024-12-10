c142d1f8c50f77808507525bcad186c3e4c8c62d
huy.nguyen <huy.nguyen8@sotatek.com>
2024-12-10 09:56:20 +0700

    update

--- src/pages/CriteriaBeforeEnteringOrders/index.jsx
+++ src/pages/CriteriaBeforeEnteringOrders/index.jsx
@@ -14,27 +14,17 @@ const criteria = [
     label: "Có xác nhận sóng hồi ở khung thời gian nhỏ hơn.",
   },
   {
-    label: "Chỉ giao dịch theo xu hướng của khung thời gian lớn.",
-  },
-  {
-    label: "Không cố tình đi đoán đỉnh, đoán đáy.",
+    label: "Không cố tình đi đoán đỉnh, đoán đáy, chỉ follow theo xu hướng chính và quan sát các dấu hiệu ở khung thời gian giao dịch.",
   },
   {
     label: "Dấu hiệu ở khung thời gian lớn sẽ có độ chính xác khá cao.",
   },
   {
-    label: "Chỉ giao dịch BTC và ETH.",
+    label: "Danh mục đầu tư chỉ nên tập trung vào BTC, ETH, SOL, BNB, LINK, SUI, XRP.",
   },
   {
     label:
-      "Trước khi đưa ra một quyết định giao dịch cần suy xét kĩ lưỡng, thẩm định kĩ càng.",
-  },
-  {
-    label: "Danh mục đầu tư nên tập trung 70% với BTC, 30% với Altcoin.",
-  },
-  {
-    label:
-      "Đối với Altcoin thì sẽ phân bổ vào mua spot khi BTC đã xác nhận bước vào sóng tăng ở khung thời gian lớn và chỉ phân bổ vào tối đa khoảng 5 đồng coin.",
+      "Đối với các đồng meme coin thì chỉ nên tận dụng Spot để lướt sóng ngắn.",
   },
 ];
 
