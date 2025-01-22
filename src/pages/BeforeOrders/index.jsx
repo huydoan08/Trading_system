4492688773e767001fda5563a3113111bbd1738e
huy.nguyen <huy.nguyen8@sotatek.com>
2025-01-22 23:19:29 +0700

    dfsa

--- src/pages/BeforeOrders/index.jsx
+++ src/pages/BeforeOrders/index.jsx
@@ -7,7 +7,7 @@ const criteria = [
   "Có chiến lược giao dịch phù hợp tùy thuộc vào từng giai đoạn của thị trường, và luôn chuẩn bị sẵn trước các kịch bản có thể xảy ra.",
   "Kiên nhẫn quan sát các dấu hiệu của thị trường trên đồ thị giá và RSI.",
   "Chỉ vào lệnh khi các tín hiệu đã đủ mạnh.",
-  "Chỉ giao dịch BTC thôi nếu muốn giữ được tiền.",
+  "Chỉ giao dịch Spot BTC thôi, còn Alt coin nếu muốn đầu tư thì cần phải biết được chắc chắn vùng nào nhóm MM nó gom và chỉ đầu tư theo sóng thôi chứ không mang tư duy hold to die.",
   "Không cố tình đi đoán đỉnh và đoán đáy.",
   "Tính toán thật kĩ khoản cắt lỗ cho mỗi lệnh giao dịch.",
   "Tuyệt đối không DCA làm cho khối lượng giao dịch lớn dần lên."
